685e651be4492d2f121ec4bd6c0c696aaf75ea89
guowei <guowei@guowei>
2020-09-06 15:19:49 +0800

    submit C-learning part

---
+++ c-example/fathm_ft.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+	int feet,fathoms;
+	fathoms=2;
+	feet = 6*fathoms;
+	printf("There are %d feet in %d famthoms!\n",feet,fathoms);
+	printf("Yes,I said %d feet!\n",6*fathoms);
+	getchar();
+	return 0;
+}
