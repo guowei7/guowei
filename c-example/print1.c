67189bf0cb2c8464f077c6b6af338623e96db68c
guowei <guowei@guowei>
2020-09-12 17:14:23 +0800

    hugo compiler

---
+++ c-example/print1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int ten = 10 ;
+    int two = 2;
+    printf("Doing it right: ");
+    printf("%d minus %d is %d\n", ten, 2, ten - two);
+    printf("Do it wrong: ");
+    printf("%d minus %d is %d\n", ten);//遗漏2个参数
+    return 0;
+}
