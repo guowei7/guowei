b08bad213245a838bcb315b16ae46fab4c4c96eb
guowei <guowei@guowei>
2021-01-09 22:57:35 +0800

    Nahan-Learning.md ADD\nC-Learning-tow.md ADD

--- c-example/test.c
+++ c-example/test.c
@@ -2,6 +2,18 @@
 #include <string.h>
 int main(void)
 {
-    printf("%s\n","123" ); 
-    return 0;
+    int a=0,i;
+    for(i=1;i<5;i++)
+    {
+    	switch(i)
+    	{
+    		case 0:
+    		case 3: a+=2;
+    		case 1:
+    		case 2: a+=3;
+    		default: a+=5;
+    	}
+    }
+    printf("%d\n",a);
+    printf("%d\n",sizeof(void *));
 }
